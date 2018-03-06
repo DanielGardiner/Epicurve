d0191c555fc8044a03199bfcfb59ea15791a3b6e
Daniel Gardiner <daniel.gardiner@phe.gov.uk>
2018-03-06 10:00:39 +0000

    added blan.background argument

--- epicurve.R
+++ epicurve.R
@@ -37,7 +37,7 @@
 epicurve = function(x, date.col, time.period, fill.by=NULL, split.by=NULL, shade.by=NULL,
                     start.at=NULL, stop.at=NULL, xlab=NULL, ylab="Incidence", 
                     fill.by.legend.title = NULL, shade.by.legend.title = NULL,
-                    angle=45, col.pal=1, label.breaks =0, epi.squares = TRUE, na.rm = FALSE) {
+                    angle=45, col.pal=1, label.breaks =0, epi.squares = TRUE, blank.background = FALSE, na.rm = FALSE) {
   
   library(ggplot2)
   library(ISOweek)
@@ -238,6 +238,18 @@ epicurve = function(x, date.col, time.period, fill.by=NULL, split.by=NULL, shade
                                             colour = "black", face="bold"),
                 legend.position="right")
   
+  # remove background if specified in blank.background argument
+  
+  if(blank.background){
+  
+    p = p + theme(panel.background = element_blank())
+    
+  } else {
+    
+    NULL
+    
+  }
+  
   # add labels for the fill.by and shade.by legends
   
   p = p + labs(fill = fill.by.legend.title,
@@ -278,7 +290,7 @@ epicurve = function(x, date.col, time.period, fill.by=NULL, split.by=NULL, shade
 } 
 
 
-################################################################################
+# ################################################################################
 # # EXAMPLE
 # 
 # # generate dummy data
@@ -304,40 +316,42 @@ epicurve = function(x, date.col, time.period, fill.by=NULL, split.by=NULL, shade
 #          start.at = "2014-08-01", stop.at = "2015-06-28",
 #          xlab="Year week", ylab="Count",
 #          fill.by.legend.title = "Sex", shade.by.legend.title = NULL, angle=90,
-#          col.pal="phe", label.breaks = 1, epi.squares = TRUE, na.rm = TRUE)
+#          col.pal="phe", label.breaks = 1, epi.squares = TRUE, blank.background = TRUE,
+#          na.rm = TRUE)  
+#   
 # 
-# epicurve(x = data, date.col = "dates.year.month", time.period = "use.date.col.as.is",
+# epicurve(x = data, date.col = "dates.year.month", time.period = "use.date.col.as.is", 
 #          fill.by="status", split.by=NULL, shade.by=NULL,
 #          start.at = "2015-02-01", stop.at = "2015-06-22",
-#          xlab="Year_week", ylab="Count",
-#          fill.by.legend.title = "", shade.by.legend.title = NULL, angle=90,
+#          xlab="Year_week", ylab="Count", 
+#          fill.by.legend.title = "", shade.by.legend.title = NULL, angle=90, 
 #          col.pal=1, label.breaks = 0, epi.squares = FALSE, na.rm = TRUE)
 # 
 # 
-# epicurve(data, date.col = "dates", time.period = "year.quarter",
+# epicurve(data, date.col = "dates", time.period = "year.quarter", 
 #          fill.by="sex", split.by=NULL, shade.by=NULL,
 #          start.at = "2014-01-01", stop.at = "2015-06-22",
-#          xlab=NULL, ylab="Count",
-#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=90,
+#          xlab=NULL, ylab="Count", 
+#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=90, 
 #          col.pal=8, label.breaks = 0, epi.squares = FALSE, na.rm = TRUE)
 # 
-# epicurve(data, date.col = "dates", time.period = "year.month",
+# epicurve(data, date.col = "dates", time.period = "year.month", 
 #          fill.by="sex", split.by=NULL, shade.by=NULL,
 #          start.at = "2014-01-01", stop.at = "2016-01-31",
-#          xlab=NULL, ylab="Count",
-#          fill.by.legend.title = "Sex", shade.by.legend.title = NULL, angle=90,
+#          xlab=NULL, ylab="Count", 
+#          fill.by.legend.title = "Sex", shade.by.legend.title = NULL, angle=90, 
 #          col.pal=2, label.breaks = 0, epi.squares = FALSE, na.rm = FALSE)
 # 
-# epicurve(data, date.col = "dates", time.period = "day",
+# epicurve(data, date.col = "dates", time.period = "day", 
 #          fill.by="sex", split.by=NULL, shade.by=NULL,
 #          start.at = "2014-01-01", stop.at = "2014-02-19",
-#          xlab=NULL, ylab="Count", fill.by.legend.title = NULL, angle=90,
+#          xlab=NULL, ylab="Count", fill.by.legend.title = NULL, angle=90, 
 #          col.pal=4, label.breaks = 3, epi.squares = FALSE, na.rm = TRUE)
 # 
-# epicurve(data, date.col = "dates.year.month", time.period = "use.date.col.as.is",
+# epicurve(data, date.col = "dates.year.month", time.period = "use.date.col.as.is", 
 #          fill.by="sex", split.by=NULL, shade.by=NULL,
 #          start.at = "2014-01-01", stop.at = "2014-02-19",
-#          xlab=NULL, ylab="Count", fill.by.legend.title = NULL, angle=90,
+#          xlab=NULL, ylab="Count", fill.by.legend.title = NULL, angle=90, 
 #          col.pal=4, label.breaks = 3, epi.squares = FALSE, na.rm = TRUE)
 # 
 # 
@@ -345,25 +359,25 @@ epicurve = function(x, date.col, time.period, fill.by=NULL, split.by=NULL, shade
 # epicurve(data, date.col = "dates", time.period = "month",
 #          fill.by="sex", split.by=NULL, shade.by="conf",
 #          start.at = "2014-01-01", stop.at = "2016-04-20",
-#          xlab=NULL, ylab="Count",
-#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=0,
+#          xlab=NULL, ylab="Count", 
+#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=0, 
 #          col.pal=6, label.breaks = 0, epi.squares = FALSE, na.rm = TRUE)
 # 
 # 
 # epicurve(data, date.col = "dates", time.period = "month",
 #          fill.by="conf", split.by="conf", shade.by="sex",
 #          start.at = "2014-01-01", stop.at = "2016-04-20",
-#          xlab=NULL, ylab="Count",
-#          fill.by.legend.title = "Confirmed status", shade.by.legend.title = "Sex",
+#          xlab=NULL, ylab="Count", 
+#          fill.by.legend.title = "Confirmed status", shade.by.legend.title = "Sex", 
 #          angle=0, col.pal="phe", label.breaks = 0, epi.squares = TRUE, na.rm = TRUE)
 # 
-# epicurve(data, date.col = "dates", time.period = "month",
+# epicurve(data, date.col = "dates", time.period = "month", 
 #          fill.by="sex", split.by="conf", shade.by=NULL,
 #          start.at = "2014-01-01", stop.at = "2016-04-20",
-#          xlab="Month", ylab="Count",
-#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=0,
+#          xlab="Month", ylab="Count", 
+#          fill.by.legend.title = NULL, shade.by.legend.title = NULL, angle=0, 
 #          col.pal=7, label.breaks = 0, epi.squares = TRUE, na.rm = TRUE)
-# 
+
 
 
 
